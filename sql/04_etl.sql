1359a1a927f4a8f332d9d38015d7115efbfa8867
BeritanAhmadzade <beritanahmadzade@gmail.com>
2026-06-04 16:57:55 +0330

    Add 04_etl.sql

--- sql/04_etl.sql
+++ sql/04_etl.sql
@@ -1,1 +1,42 @@
 -- ETL Process
+CREATE TABLE DimPatient(
+	Patientkey INT PRIMARY KEY,
+	PatientID INT,
+	FullName VARCHAR(100),
+	Gender VARCHAR(10)
+);
+
+CREATE TABLE DimDoctor(
+	DoctorKey INT PRIMARY KEY,
+	DoctorID INT,
+	FullName VARCHAR(100),
+	Specialty VARCHAR(100)
+);
+
+CREATE TABLE DimDepartment (
+	DepartmentKey INT PRIMARY KEY,
+	DepartmentID INT,
+	DepartmentName VARCHAR(100)
+);
+
+CREATE TABLE DimDate(
+	DateKey INT PRIMARY KEY,
+	FullDate DATE,
+	Year INT,
+	Month INT
+);
+
+CREATE TABLE FactAppointments (
+	AppointmentsKey INT PRIMARY KEY,
+	PatientKey INT,
+	DoctorKey INT,
+	DateKey INT,
+	status VARCHAR(20)
+);
+
+CREATE TABLE FactBilling(
+	BiilKey INT PRIMARY KEY,
+	AppointmentKey INT,
+	DateKey INT,
+	Amount DECIMAL(10,2)
+);
