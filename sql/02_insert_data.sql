e94d8b6115556964c9dbf3fca2396cb2185a632e
BeritanAhmadzade <beritanahmadzade@gmail.com>
2026-06-04 16:40:45 +0330

    Update 02_insert_data.sql

--- sql/02_insert_data.sql
+++ sql/02_insert_data.sql
@@ -11,19 +11,26 @@ insert into Doctors values
 (3,'Dr.John Smith','Emergency',3);
 
 
-insert into Patints values 
+
+
+insert into Patients values 
 (1,'Ali Ahmadi','Male','1380-05-01'),
 (2,'Sara Ahmadzade','Female','1365-05-23'),
-(3,'Reza Karimi','Male','1378-02-24');
+(3,'Reza Karimi','Male','1378-02-24'),
+(4,'Saba Mohtasham','Male','1376-02-22');
+
+
 
 insert into Appointments values 
 (1,1,1,'1405-01-10','Completed'),
 (2,2,2,'1405-02-22','Completed'),
-(3,3,3,'1405-03-04','Canceled'),
-(4,4,4,'1405-03-05','Completed');
+(3,3,3,'1405-03-04','Canceled');
 
 
 insert into Billing values
 (1,1,120.00),
-(2,2,150.00),
-(3,4,200.00);
+(2,2,150.00);
+
+
+
+
